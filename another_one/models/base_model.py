de4440988660c3c63634fd2cfba2ce59f301e075
Martin Kiilu <martinkiilu001@gmail.com>
2023-04-15 01:10:13 -0700

    another_one

---
+++ another_one/models/base_model.py
@@ -0,0 +1,33 @@
+from uuid import uuid4
+from datetime import datetime
+
+# Define BaseModel Class.
+class BaseModel:
+    """Defines all common attributes/methods for other classes."""
+
+    def __init__(self):
+        """ Initialize BaseModel Instance"""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+    
+    def __str__(self):
+        """Returns string representation of object"""
+        return f"{type(self).__name__} ({self.id}) {str(self.__dict__)}"
+        
+    def save(self):
+        """records update time"""
+        self.updated_at = datetime.now()
+    
+    def to_dict(self):
+        """Return dictionary of the BaseModel Instance."""
+        diction = {}
+        diction = (self.__dict__).copy()
+        diction["__class__"] = self.__class__.__name__
+        diction["created_at"] = self.created_at.isoformat()
+        diction["updated_at"] = self.updated_at.isoformat()
+        return diction
+    def __str__(self):
+        """Returns string representation of object"""
+        clssname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clssname, self.id, self.__dict__)
